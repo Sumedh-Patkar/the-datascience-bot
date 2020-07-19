a54fb416163da98baa8520d821c02ce999c27497
Sumedh Patkar <sumedh.patkar19@gmail.com>
2020-07-19 22:28:14 +0530

    Sample config.js file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:APPLICATION_CONSUMER_KEY_HERE,
+    consumer_secret:APPLICATION_CONSUMER_SECRET_HERE,
+    access_token:ACCESS_TOKEN_HERE,
+    access_token_secret:ACCESS_TOKEN_SECRET_HERE
+}
